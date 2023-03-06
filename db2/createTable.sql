34764ce85f34494c5d9c2bde67c24316fb5e6c90
hector <hectortoraltrabajos@gmail.com>
2023-03-06 19:09:35 +0100

    oracle xe

--- db2/createTable.sql
+++ db2/createTable.sql
@@ -9,7 +9,7 @@ CREATE TABLE books (
 
 CREATE TABLE users (
    nombre           VARCHAR(255)    NOT NULL,
-   fechaNaciemiento TIMESTAMP       NOT NULL,
+   fechaNacimiento TIMESTAMP       NOT NULL,
    DNI              VARCHAR(9)      PRIMARY KEY,
    genero           VARCHAR(6)      NOT NULL
 );
