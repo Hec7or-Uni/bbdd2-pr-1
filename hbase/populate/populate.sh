0d17c0b767cf66985ad111ba89fd22cd9d1a714e
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2023-03-07 22:43:32 +0100

    Mejoras en hbase

--- hbase/populate/populate.sh
+++ hbase/populate/populate.sh
@@ -1,5 +1,4 @@
 hbase shell < createTable.hbase
-./users.sh
 hbase shell < users.hbase
 hbase shell < MOCK_DATA_BOOKS.hbase
 hbase shell < MOCK_DATA_USERS.hbase
--- hbase/populate/users.sh
+++
@@ -1,5 +0,0 @@
-adduser developer1 --gecos ",,," --disabled-password
-echo "developer1:password" | chpasswd
-
-adduser client1 --gecos ",,," --disabled-password
-echo "client1:password" | chpasswd
